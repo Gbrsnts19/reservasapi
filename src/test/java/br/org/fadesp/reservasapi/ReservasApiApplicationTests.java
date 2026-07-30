729bbb157710607d4ada71093841fa6eb3a4e79d
Gabriel de Oliveira Carvalho Santos <gbrsnts19@gmail.com>
2026-07-30 20:01:47 -0300

    chore: inicializa projeto Spring Boot com Java 17 e H2

---
+++ src/test/java/br/org/fadesp/reservasapi/ReservasApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.org.fadesp.reservasapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReservasApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
